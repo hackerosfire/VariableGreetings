8d2b657abf058b3c8bc5f97624b8c8f8cc77fd3e
hackerosfire <v6234@abv.bg>
2017-02-13 14:23:53 +0200

    Add project files.

---
+++ VariableGreetings/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariableGreetings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double inputAge = double.Parse(Console.ReadLine());
+            string inputGender = Console.ReadLine();
+            if (inputGender == "f" && inputAge < 16)
+            {
+                Console.WriteLine("Miss");
+            }
+            else if (inputGender == "f" && inputAge >= 16)
+            {
+                Console.WriteLine("Ms.");
+            }
+            else if (inputGender == "m" && inputAge < 16)
+            {
+                Console.WriteLine("Master");
+            }
+            else
+            {
+                Console.WriteLine("Mr.");
+            }
+        }
+    }
+}
